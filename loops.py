2056d9c04bb6e4f11087af2df47e7c748544cb2b
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-02 10:25:06 +0530

    adding oy file

---
+++ loops.py
@@ -0,0 +1,16 @@
+#loop
+list1=["Biscuit", "oil", "Soap", "Utensils"]
+for item in list1:
+    print(item)
+
+#while loop
+i=0
+while(i<50):
+    print(i)
+#it will not stop till we stop it
+
+#how to stop while loop
+i=0
+while(i<50):
+    print(i)
+    i=i+1
---
+++ specificdatatype.py
@@ -0,0 +1,6 @@
+x= 5
+a = str(x)
+print(type(a))
+
+x=str(5)
+print(type(x))
