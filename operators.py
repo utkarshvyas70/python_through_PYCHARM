56cd7b92658451084fe8bfd6ca7f937311b40979
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-02 11:05:03 +0530

    adding py file

---
+++ operators.py
@@ -0,0 +1,36 @@
+#1.Arithmetic operators(+,-,*,/,**,//)
+print("5+6 is", 5+6)
+print("5*6 is", 5*6)
+print("5**6 is", 5**6)
+print("5//6 is", 5//6)
+
+#2.Assignment operators(+=,-+, /=)
+#a.+=
+x=5
+print(x)
+x+=7
+print(x)
+#b./=
+x=12
+print(x)
+x/=2
+print(x)
+
+#3.Comparison operators(==,!=,<=,>=)
+x=5
+print(x==5)
+print(x!=5)
+print(x<=5)
+print(x>=5)
+
+#4.Logical operators(and,or)
+a= True
+b= False
+print(a and b)
+print(a or b)
+
+#5. Identity operators(is, is not)
+a= 5
+b= 6
+print(a is b)
+print(a is not b)
