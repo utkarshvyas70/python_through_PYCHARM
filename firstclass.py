cb9fb69746633ef6ea7665a237c437f0ef727d85
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-02 10:12:01 +0530

    adding py file

---
+++ firstclass.py
@@ -0,0 +1,32 @@
+"""create two variables and assign values to them
+create a third variable and add x and y
+print the sum"""
+x=4
+y=6
+c=(x+y)
+print("The sum is",c)
+
+
+
+#ghodo ko nahi mil rhi ghas , ghadhe kha rhe chamanprash.
+#amother way of making adding calculator
+x=input("Enter your first number:")
+y=input("enter your second number:")
+sum= int(x)+int(y)
+print(sum)
+
+
+
+#subtraction
+x=input("Enter your first number:")
+y=input("enter your second number:")
+subtraction= int(x)-int(y)
+print(subtraction)
+
+
+#pseudo code
+#Take inputs from user for two numbers (x and y)
+a=input("Enter your first number")
+b=input("Enter your second number")
+a,b=b,a
+print(a,b)
