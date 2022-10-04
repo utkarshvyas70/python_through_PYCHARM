e1b518b1cf3921e1c716e2eb5c13bf64544927e1
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-04 09:45:32 +0530

    adding py file

---
+++ math.py
@@ -0,0 +1,26 @@
+#built-in math function
+#. the min() and max() function
+x=min(1,2,4)
+y=max(5,6,7)
+print(x)
+print(y)
+
+#2.the abs() abstract function returns the possible value of negative number
+x=abs(-33)
+print(x)
+
+#3. the pow() power function
+x=pow(2,2)
+print(x)
+
+#Math module
+import math
+x=math.sqrt(64)
+print(x)
+#A. math.ceil() returns the upward nearest integer
+#B. math.floor() return the downward nearest integer
+import math
+x=math.ceil(4.6)
+y=math.floor(5.6)
+print(x)
+print(y)
