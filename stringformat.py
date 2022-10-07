50ea02766ddf78eadf3ce0e53001599da125d363
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-07 16:00:26 +0530

    adding py file

---
+++ stringformat.py
@@ -0,0 +1,10 @@
+price=49
+txt="The price is {} dollars"
+print(txt.format(price))
+
+#want to add more values
+quantity=3
+itemno=567
+price=49
+myorder="I want {} pieces of item number {} for {} dollars"
+print(myorder.format(quantity,itemno,price))
