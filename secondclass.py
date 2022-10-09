471dfe8ce8d9fa5b39820912be79a4bf40aff1a0
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-09 13:09:17 +0530

    adding py file

---
+++ secondclass.py
@@ -0,0 +1,15 @@
+#if and else
+a=5
+b=10
+if a == b:
+    print("True")
+else:
+    print("Incorect condition")
+
+#Question
+x=int(input("Enter your firsr Number"))
+y=int(input("Enter your second number"))
+if x < y:
+    print("YAYY")
+else:
+    print("NAYY")
