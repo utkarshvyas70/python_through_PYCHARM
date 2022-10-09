b586343ef85b060789e2e33c060c02e0a685c820
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-09 13:30:12 +0530

    adding py file

--- statementquestion.py
+++ statementquestion.py
@@ -1,3 +1,7 @@
+#A company decided to give bonus of 1000rs to
+#employee if his/her service is more than 5 years
+#ask user their salary and year of service and print
+#the net bonus amout
 year=float(input("How many years you worked:"))
 salary=float(input("Your current salary you are getting: "))
 if year > 5:
