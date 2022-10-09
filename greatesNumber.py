c25e88758da6b2a8aaf938fe476ed4a76a35c91e
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-09 15:58:15 +0530

    adding py file

---
+++ greatesNumber.py
@@ -0,0 +1,8 @@
+number1=int(input("Enter your number"))
+number2=float(input("Enter your number"))
+if number1>number2:
+    print("number1 is greater")
+elif number2>number1:
+    print("number2 is greater")
+else:
+    print("Invalid number")
