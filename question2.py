9ef07981fd29f0cf9a6a9ea11010832cfdbb7ce1
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-09 15:31:06 +0530

    adding py file

---
+++ question2.py
@@ -0,0 +1,10 @@
+marks=float(input("Enter your marks"))
+# why (:and")-inside our if there are 2 conditions
+if marks>90 and marks<=100:
+    print("A")
+elif marks >80 and marks<=90:
+    print("B")
+elif marks>70 and marks<=80:
+    print("C")
+else :
+    print("Invalid Number")
