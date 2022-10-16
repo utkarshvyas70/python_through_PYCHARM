fa363106e139950658c47eb8c1936f2fda519250
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-16 12:14:56 +0530

    adding py file

---
+++ question9.py
@@ -0,0 +1,8 @@
+#you have to take a user input check divisible by 3 and 2 both
+a=int(input("Enter your number:"))
+if a%2==0 :
+    print("yes it is divisible by 2")
+elif a%3==0:
+    print("yes it is divisible 3")
+else:
+    print("invalid error")
