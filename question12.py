49efdcd0be8cad4f677f64f030eeaa645e2666fa
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-16 16:07:44 +0530

    adding py file

---
+++ question12.py
@@ -0,0 +1,9 @@
+a=(input("Enter the vowels:"))
+b="A,E,I,O,U"
+c="a,e,i,o,u"
+if a in b :
+    print("Yes it is a vowel")
+elif a in c :
+    print("Yes it is a vowel")
+else:
+    print("It is not a vowel")
