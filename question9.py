efdce9689de38f22d9ea47777e2a0e33c29b0f9e
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-16 12:29:49 +0530

    adding py file

--- question9.py
+++ question9.py
@@ -4,5 +4,7 @@ if a%2==0 :
     print("yes it is divisible by 2")
 elif a%3==0:
     print("yes it is divisible 3")
+elif a%2==0 and a%3==0:
+    print("It is divisible by both")
 else:
     print("invalid error")
