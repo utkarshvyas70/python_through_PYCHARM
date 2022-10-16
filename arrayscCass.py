9c34d201e2b0806ed69bef218f4600e513470c31
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-10-16 13:50:44 +0530

    adding py file

---
+++ arrayscCass.py
@@ -0,0 +1,12 @@
+b="Utkarsh Vyas"
+a="Hello World"
+print(a[6])
+print(a[3])
+print(b[8])
+print(a[-3])
+print(len(a+b))
+print(len(b))
+if "yas" not in b:
+    print("Yes it is ")
+else:
+    print("bhag bsdk")
