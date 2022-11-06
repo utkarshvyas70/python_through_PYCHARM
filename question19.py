6c69011269146df2bf5c79ade14fa96a95874058
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-06 16:07:07 +0530

    aadding py file

---
+++ question19.py
@@ -0,0 +1,15 @@
+"""create a function that will display the how many items in the list have names more tan 5 characters
+
+names=["utkarsh","ankit","ramendra","adhiraj"]"""
+
+def count(list):
+    total=0
+    for i in list:
+        if len(1)>5:
+            total=total+1
+        else:
+            total=total
+        return total
+list=[]
+total=count(list)
+print(total)
