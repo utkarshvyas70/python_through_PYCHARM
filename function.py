400d4d5f686a752955594803e3351ca3ef99830a
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-06 13:03:29 +0530

    adding py file

--- function.py
+++ function.py
@@ -4,7 +4,39 @@ def boy():
     print("Girl")
 #calling a function
 boy()"""
-def add(num1,num2):
+
+
+"""def add(num1,num2):
     subtract=num1-num2
     print(subtract)
-add(5,6)
+add()"""
+
+
+"""def multiply(num1,num2):
+    multiply=num1*num2
+    return multiply
+x=multiply(1,2)
+print(x)"""
+
+
+"""def name(firstname):
+    print("My name is",firstname)
+name("Fati")
+name("Bhosdi")"""
+
+"""def name(firstName, lastName):
+    print("my name is",firstName + " " + lastName)
+name("Utkarsh", "Vyas")"""
+
+"""def name(*args):
+    print(args)
+name("Utkarsh", "Vyas")"""
+
+
+def name(name, *args):
+    print(name)
+    print(args)
+name("Utkarsh","Vyas")
+
+
+
