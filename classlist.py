24059946af4b6c9d09ec888b3c23b34c4872953a
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-12 16:46:33 +0530

    adding py file

--- classlist.py
+++ classlist.py
@@ -1,4 +1,4 @@
-colors=["Red","Blue","Red","Black",55]
+'''colors=["Red","Blue","Red","Black",55]
 print(colors)
 
 colors=["Red","Blue","Red","Black",55]
@@ -31,3 +31,71 @@ print(colors)
 colors=["Red","Blue","Red","Black",55]
 colors.remove("Red")
 print(colors)
+
+colors=["Red","Blue","Red","Black"]
+cars=["Tata","Bmw"]
+for x in range(len(cars)):
+    print(cars[x])
+while x<len(cars):
+    print(cars[x])
+    x+=1
+
+
+colors=["Red","Blue","Red","Black"]
+cars=["Tata","Bmw"]
+[print(x) for x in cars]
+
+list=[]
+cars=["Tata","Bmw"]
+for i in cars:
+    if "a" in i:
+        list.append(i)
+print(list)
+
+colors=["Red","Blue","Red","Black"]'''
+cars=["Tata","Bmw"]
+
+list=[x for x in cars if x=="Tata"]
+print(list)
+
+list=[x for x in cars if x!="Tata"]
+print(list)
+
+colors=["Red","Blue","Red","Black"]
+cars=["Tata","Bmw"]
+numbers=[1,4,6,8,10]
+cars.sort()
+numbers.sort()
+print(cars)
+
+colors=["Red","Blue","Red","Black"]
+cars=["Tata","Bmw"]
+numbers=[1,4,6,8,10]
+cars.sort()
+numbers.sort()
+print(cars)
+
+
+numbers=[1,2,3,4,5,6,7]
+squares=[]
+for i in numbers:
+    squares.append(i**2)
+print(squares)
+
+numbers=[1,2,3,4,5,6,2,8,9]
+for i in range(0,len(numbers)-1):
+    if numbers[i]==2:
+        numbers.pop(i)
+        numbers.insert(i,200)
+print(numbers)
+
+indx=numbers.index(2)
+numbers[indx]=200
+print(numbers)
+
+list1=["x","y","z"]
+list2=[1,2,3]
+for x in list2:
+    list1.append(x)
+list3=list1+list2
+print(list3)
