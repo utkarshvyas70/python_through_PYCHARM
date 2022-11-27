7c3a6677a1ab406fdf5aca5b3576112473add7e3
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-27 12:33:31 +0530

    Ramendra ki shaadi

---
+++ 27nov.py
@@ -0,0 +1,3 @@
+Groom=str(input("Enter the name of the Groom: \n"))
+Bride=str(input("Enter the name of the Bride: \n"))
+print(f"Congratulations Mr.{Groom} and Mrs.{Bride}, you are now happily married ")
