fa9d5fdae84230e6253b6f0ef1735f9dae8ccd35
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-27 16:37:43 +0530

    Sets

---
+++ 27novsets.py
@@ -0,0 +1,11 @@
+# sets - store multiple variables in single one
+
+# myset={"car","boat","train"}
+# if "boat" in myset:
+#     print("boat")
+# else:
+#     print(myset)
+
+tuple1=('H','E','L','L','O')
+for i in tuple1:
+    print(i,end="")
