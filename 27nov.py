09264f1f8b538045fbbba9f514ec5a820d1cc6f1
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-11-27 16:15:25 +0530

    Class

--- 27nov.py
+++ 27nov.py
@@ -1,38 +1,81 @@
 # Wedding code
-Groom=str(input("Enter the name of the Groom: \n"))
-Bride=str(input("Enter the name of the Bride: \n"))
-print(f"Congratulations Mr.{Groom} and Mrs.{Bride}, you are now happily married ")
+# Groom=str(input("Enter the name of the Groom: \n"))
+# Bride=str(input("Enter the name of the Bride: \n"))
+# print(f"Congratulations Mr.{Groom} and Mrs.{Bride}, you are now happily married ")
 
-tuple2=("fkjefij",1,2,3,4,5)
-print(len(tuple))
-print(tuple[:3])
-list1=list(tuple2)
-list1.append(5)
-print(list1)
-tuple1=tuple(list1)
-print(tuple1)
+# tuple2=("fkjefij",1,2,3,4,5)
+# print(len(tuple))
+# print(tuple[:3])
+# list1=list(tuple2)
+# list1.append(5)
+# print(list1)
+# tuple1=tuple(list1)
+# print(tuple1)
 
-tuple1=(10,20,30,40)
-list1=list(tuple1)
-list1.reverse()
-tuple2=tuple(list1)
-print(tuple2)
+# tuple1=(10,20,30,40)
+# list1=list(tuple1)
+# list1.reverse()
+# tuple2=tuple(list1)
+# print(tuple2)
 
-tuple1=("Utkarsh" ,"Vyas")
-print(tuple1)
-del tuple1
+# tuple1=("Utkarsh" ,"Vyas")
+# print(tuple1)
+# del tuple1
 
-tuple1=(1,2,3,[6,7],4,5)
-tuple1[3][0]=8
-print(tuple1)
+# tuple1=(1,2,3,[6,7],4,5)
+# tuple1[3][0]=8
+# print(tuple1)
 
-tuple1=(10,20)
-tuple2=(30,40)
-tuple1,tuple2=tuple2,tuple1
-print(tuple1)
-print(tuple2)
+# tuple1=(10,20)
+# tuple2=(30,40)
+# tuple1,tuple2=tuple2,tuple1
+# print(tuple1)
+# print(tuple2)
 
 
+# tuple1=(1,2,3)
+# (one, two, three)=tuple1
+# print(one)
+# print(two)
+# print(three)
+
+# tuple1=(1,2,3,4,5,6,7,8)
+# (one, *two, three)=tuple1
+# print(one)
+# print(two)
+# print(three)
+
+# tuple1=(1,2,3,4)
+# (one, two, three, four)=tuple1
+# a=one
+# b=two
+# c=three
+# d=four
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+# tuple1=(1,2,3,4)
+# for i in tuple1:
+#     print(i)
+
+# tuple1=(1,2,3,4)
+# list1=list(tuple1)
+# tuple2=list1[1,2]
+# print(tuple2)
+
+
+# x=(1)
+# print(type(x))
+
+# x=str(input())
+# y=str(input())
+# z=str(input())
+# d=str(input())
+# list1=(x,y,z,d)
+# tuple1=tuple(list1)
+# print(tuple1)
 
 
 
