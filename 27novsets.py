0d1f6fa138710f2f28b1b7854f904d3a7e846c96
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-12-04 12:36:34 +0530

    py file

--- 27novsets.py
+++ 27novsets.py
@@ -1,11 +1,24 @@
 # sets - store multiple variables in single one
 
-myset={"car","boat","train"}
-if "boat" in myset:
-    print("boat")
-else:
-    print(myset)
+myset1={"car","boat","train"}
+myset2={1,2,3}
+myset3={3,5,6}
+# if "boat" in myset:
+#     print("boat")
+# else:
+#     print(myset)
+#
+# tuple1=('H','E','L','L','O')
+# for i in tuple1:
+#     print(i,end="")
 
-tuple1=('H','E','L','L','O')
-for i in tuple1:
-    print(i,end="")
+# myset1.add('kkk')
+# myset1.update(myset2)
+# myset1.discard("car")
+# myset1.remove("caro")
+# output=myset2.union(myset3)
+# output=myset2.intersection(myset3)
+# output=myset2.symmetric_difference(myset3)
+output=myset3
+print(output)
+
