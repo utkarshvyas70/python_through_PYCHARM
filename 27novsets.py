07a4c04cbe679e3f650583c3f04db068cfa91acd
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-12-04 12:45:14 +0530

    py file

--- 27novsets.py
+++ 27novsets.py
@@ -3,22 +3,22 @@
 myset1={"car","boat","train"}
 myset2={1,2,3}
 myset3={3,5,6}
-# if "boat" in myset:
-#     print("boat")
-# else:
-#     print(myset)
-#
-# tuple1=('H','E','L','L','O')
-# for i in tuple1:
-#     print(i,end="")
+if "boat" in myset1:
+    print("boat")
+else:
+    print(myset1)
 
-# myset1.add('kkk')
-# myset1.update(myset2)
-# myset1.discard("car")
-# myset1.remove("caro")
-# output=myset2.union(myset3)
-# output=myset2.intersection(myset3)
-# output=myset2.symmetric_difference(myset3)
+tuple1=('H','E','L','L','O')
+for i in tuple1:
+    print(i,end="")
+
+myset1.add('kkk')
+myset1.update(myset2)
+myset1.discard("car")
+myset1.remove("caro")
+output=myset2.union(myset3)
+output=myset2.intersection(myset3)
+output=myset2.symmetric_difference(myset3)
 output=myset3
 print(output)
 
