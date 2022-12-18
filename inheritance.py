47b0472d338b12393205f286c17550b566a0b9df
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-12-18 16:48:16 +0530

    py file

--- inheritance.py
+++ inheritance.py
@@ -1,22 +1,25 @@
 class A:
-    def __init__(self):
-        print("This is init method of A")
-    def method1(self):
-        print("This is method 1")
+     def __init__(self):
+         print("This is init method of A")
+     def method1(self):
+         print("This is method 1")
 class B:
-    def method2(self):
+     def method2(self):
         print("This is a method 2")
-# class B(A):
-#     def method2(self):
-#         print("This is a inherited method")
-# class C(B):
-#     def method3(self):
-#         print("This is method 3")
+class B(A):
+     def method2(self):
+         print("This is a inherited method")
+class C(B):
+     def method3(self):
+         print("This is method 3")
 class C(A,B):
-    def method4(self):
-        print("This is a multiple inheritance")
+     def method4(self):
+         print("This is a multiple inheritance")
 
 
 obj1=A()
 obj2=B()
 obj3=C()
+
+
+
