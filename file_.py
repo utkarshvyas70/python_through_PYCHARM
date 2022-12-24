43c06e26360db03ec671bfb1c14c805469947521
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2022-12-24 15:28:14 +0530

    py file

---
+++ file_.py
@@ -0,0 +1,18 @@
+# f=open('img.png.png','rb')
+#
+# f1=open('img_copy.png','wb')
+#
+# print(f.read())
+#
+#
+# for i in f:
+#     f1.write(i)
+
+# import os
+#
+# if os.path.exists('demofile.txt.png'):
+#     os.remove('demofile.txt.png')
+# else:
+#     print('File does not exist')
+#
+# os.remove('demofile.txt.py')
